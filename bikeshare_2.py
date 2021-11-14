d39fab62ea06b4ae458171d4b3527aff078bf60d
Reema AlGhamdi <ReemanAlGhamdi.89@gmail.com>
2021-11-15 00:30:24 +0300

    docs: Added comments on line 5 in the bikeshare_2 python file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-#data used
+#data used, lists and dictionaries
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv'}
